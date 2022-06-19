88d9b7a9fcfb1e237c4e5a5782635c77b7ba7fef
Justin Jerez <56321995+Jerez09@users.noreply.github.com>
2022-06-19 08:26:10 -0400

    Delete book method (#3)

--- BookList/Pages/Books/Index.cshtml.cs
+++ BookList/Pages/Books/Index.cshtml.cs
@@ -1,4 +1,5 @@
 using BookList.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
@@ -21,6 +22,25 @@ namespace BookList.Pages.Books
         public async Task OnGet()
         {
             Books = await _db.Book.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            // Find the book
+            var book = await _db.Book.FindAsync(id);
+
+            // Check if the book was fouund
+            if(book == null)
+            {
+                return NotFound();
+            }
+
+            // Remove the book from the database
+            _db.Book.Remove(book);
+            await _db.SaveChangesAsync();
+
+            // Return to back to the list
+            return RedirectToAction("Index");
         }
     }
 }
