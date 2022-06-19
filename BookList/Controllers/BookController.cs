9114a6c8fa7ed4dc89d797d045debd4331907fd8
Justin Jerez <jrz.09@hotmail.com>
2022-06-19 10:34:45 -0400

    GetBooks method in the api & implementation of datatables

---
+++ BookList/Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using BookList.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace BookList.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data =_db.Book.ToList() });
+        }
+    }
+}
---
+++ BookList/wwwroot/js/books.js
@@ -0,0 +1,38 @@
+﻿var dataTable;
+
+$(document).ready(() => {
+    loadDataTable();
+});
+
+const loadDataTable = () => {
+    dataTable = $('#dt_load').DataTable({
+        "ajax": {
+            "url": "/api/books",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "20%" },
+            { "data": "author", "width": "15%" },
+            { "data": "description", "width": "40%" },
+            { "data": "isbn", "width": "10%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="text-center">
+                            <a href="/Books/Edit?id=${data}" class="btn btn-success text-white">Edit</a>
+                            &nbsp;
+                            <a class="btn btn-danger text-white">Delete</a>
+                        </div>
+                    `
+                },
+                "width": "15%"
+            }
+        ],
+        "language": {
+            "emptyTable": "No data found."
+        },
+        "width": "100%"
+    });
+}
