5e7ab9dd52bd3c0a75cdb981d879603285545b45
Justin Jerez <56321995+Jerez09@users.noreply.github.com>
2022-06-20 13:49:54 -0400

    Adding MVC approach to the solution and creating Book model and ApplicationDbContext (#5)

---
+++ BookListMVC/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookListMVC.Models
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ BookListMVC/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookListMVC.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        public string Description { get; set;  }
+
+        [Required]
+        public string ISBN { get; set; }
+
+    }
+}
